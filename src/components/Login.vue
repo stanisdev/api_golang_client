e4ce26d9d22d866cf7aebf9d9f1377e637c2c8ab
Zavalishin Stanislav <zavalishin.stanislav@softomate.com>
2018-09-18 17:31:10 +0700

    Login page - in progress

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,76 @@
 <template>
   <div>
-    <h2>Login Page</h2>
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
+      <h2>Login Page</h2>
+
+      <el-form-item label="Username" prop="username">
+        <el-input v-model="ruleForm.username" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item label="Password" prop="pass">
+        <el-input v-model="ruleForm.pass" type="password" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="submitForm" :loading="formSending">Login</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
-  name: 'Notifications',
+  name: 'Login',
   data () {
-    return {}
+    const validatePass = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error('Please input the password'))
+      } else {
+        callback()
+      }
+    }
+    const validateUsername = (role, value, callback) => {
+      if (!value) {
+        callback(new Error('Please input the username'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      formSending: false,
+      ruleForm: {
+        username: '',
+        pass: ''
+      },
+      rules: {
+        pass: [
+          { validator: validatePass, trigger: 'blur' }
+        ],
+        username: [
+          { validator: validateUsername, trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm () {
+      this.$refs['ruleForm'].validate((valid) => {
+        if (valid) {
+          this.formSending = true
+          Vue.axios
+            .get('https://yesno.wtf/api')
+            .then((res) => {
+              console.log(res)
+              this.formSending = false
+            })
+            .catch(console.log)
+        } else {
+          return false
+        }
+      })
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
